a438aed7e59c9f3fab2af53b4954d854b3d6402c
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-07-05 17:46:24 +0300

    update

--- resources/views/app_footer.blade.php
+++ resources/views/app_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="container">
         <p>&copy; {{ date('Y') }} All rights reserved</p>
-        <span class="made_in">MADE WITH LOVE <a href="https://vk.com/dmitrieffserzh" title="dmitrieffserzh">DMITRIEFFSERZH</a></span>
+        <a href="https://vk.com/dmitrieffserzh" title="dmitrieffserzh" class="made_in">MADE WITH LOVE DMITRIEFFSERZH</a>
     </div>
 </footer>
