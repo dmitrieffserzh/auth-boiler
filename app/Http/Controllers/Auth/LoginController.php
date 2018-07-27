3d6118cb3583c2d9a56666a8ccef2cd534035f99
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-07-27 17:23:56 +0300

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use Auth;
+use Illuminate\Support\Facades\Session;
 use Socialite;
 use App\User;
 use App\Models\Profile;
@@ -39,26 +40,21 @@ class LoginController extends Controller {
 
 	public function callback( $service ) {
 
-		if ($service == 'google') {
+		if ( $service == 'google' ) {
 
 
-		} elseif ($service == 'twitter') {
+		} elseif ( $service == 'twitter' ) {
 
 
-		} elseif ($service == 'facebook') {
+		} elseif ( $service == 'facebook' ) {
 
-		} elseif ($service == 'vkontakte') {
-
+		} elseif ( $service == 'vkontakte' ) {
+			$user = Socialite::with( $service )->fields( [ 'first_name', 'last_name', 'email', 'sex', 'bdate', 'city', 'photo_max_orig' ] )->user();
 		}
 
 
+		//dd($user);
 
-
-		$user = Socialite::with( $service )->fields([
-			'first_name', 'last_name', 'email', 'sex', 'bdate', 'city', 'photo_max_orig'
-		])->user();
-
-		dd($user);
 
 		$authUser = $this->findOrCreateUser( $user, $service );
 
@@ -70,56 +66,64 @@ class LoginController extends Controller {
 
 	public function findOrCreateUser( $user, $service ) {
 
-		$authUser = SocialLogin::where('provider', $service )->where('provider_id', $user->id )->first();
-//dd($authUser);
+		$authUser = SocialLogin::where( 'provider', $service )
+		                       ->where( 'provider_id', $user->id )
+		                       ->first();
+		//dd($authUser);
 
 		if ( $authUser ) {
 			return $authUser->user;
 		}
 
 		// CREATE USER
-		$authUser = User::create([
-			'name' => mb_strtolower($user->nickname),
-			'email' => mb_strtolower($user->nickname).'@test.ru',
-			'password' => bcrypt(substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(6/strlen($x)) )),1,6)),
-		]);
+		$authUser = User::create( [
+			'name'     => mb_strtolower( $user->nickname ),
+			'email'    => mb_strtolower( $user->nickname ) . '@test.ru',
+			'password' => bcrypt( substr( str_shuffle( str_repeat( $x = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil( 6 / strlen( $x ) ) ) ), 1, 6 ) ),
+		] );
 
-		$socialLogin = new SocialLogin();
-		$socialLogin->provider     = $service;
-		$socialLogin->provider_id  = $user->id;
-		$socialLogin->token        = $user->token;
+		$socialLogin              = new SocialLogin();
+		$socialLogin->provider    = $service;
+		$socialLogin->provider_id = $user->id;
+		$socialLogin->token       = $user->token;
 
 		$authUser->socialLogin()->save($socialLogin);
 
 		// CREATE USER PROFILE
 
-		if ($user->getAvatar()) {
-			$filename = "/uploads/avatars/".time().".jpg";
+		if ( $user->getAvatar() ) {
+			$filename = "/uploads/avatars/" . time() . ".jpg";
 			// The filename to save in the database.
 			file_put_contents(
 				$filename,
-				file_get_contents($user->getAvatar())
+				file_get_contents( $user->getAvatar() )
 			);
 		}
 
-		if($file = $user->getAvatar()) {
-			if ($service == 'google') {
-				$file = str_replace('?sz=50', '', $file);
-			} elseif ($service == 'twitter') {
-				$file = str_replace('_normal', '', $file);
-			} elseif ($service == 'facebook') {
-				$file = str_replace('type=normal', 'type=large', $file);
+		if ( $file = $user->getAvatar() ) {
+			if ( $service == 'google' ) {
+				$file = str_replace( '?sz=50', '', $file );
+			} elseif ( $service == 'twitter' ) {
+				$file = str_replace( '_normal', '', $file );
+			} elseif ( $service == 'facebook' ) {
+				$file = str_replace( 'type=normal', 'type=large', $file );
 			}
 		}
 
 
-
-		$profile = new Profile();
+		$profile         = new Profile();
 		$profile->avatar = $filename;
-		$authUser->profile()->save($profile);
+		$authUser->profile()->save( $profile );
 
 		return $authUser;
 
 	}
 
+
+	public function logout() {
+		Auth::logout();
+		Session::flush();
+
+		return redirect( '/' );
+	}
 }
